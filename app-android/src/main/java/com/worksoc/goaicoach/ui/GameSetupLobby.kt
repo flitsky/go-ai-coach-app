b82417317a792dc857c7a61544efe734903c2e1d
Ryan9Kim <flit9sky@gmail.com>
2026-07-17 08:44:25 +0900

    Apply navigationBarsPadding to GameSetupLobby button container to prevent softkey overlap

--- app-android/src/main/java/com/worksoc/goaicoach/ui/GameSetupLobby.kt
+++ app-android/src/main/java/com/worksoc/goaicoach/ui/GameSetupLobby.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -147,6 +148,7 @@ internal fun GameSetupLobby(
             modifier = Modifier
                 .fillMaxWidth()
                 .background(MaterialTheme.colorScheme.surface)
+                .navigationBarsPadding()
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
